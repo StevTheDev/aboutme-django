f25bf24d042be16eab46949ee3c84a36af326cf6
Steven Fitzpatrick <stevthedev@gmail.com>
2019-04-06 11:51:08 -0500

    Added Static Root to settings

--- aboutme/aboutme/settings.py
+++ aboutme/aboutme/settings.py
@@ -121,6 +121,7 @@ USE_TZ = True
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR,'media')
 
+STATIC_ROOT = '/home/stev/AboutMe/static/'
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR,'static'),]
 
