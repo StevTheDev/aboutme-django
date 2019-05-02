a3c0d36107f660c4405ff5bc0908867b4e9e4f88
Steven Fitzpatrick <stevthedev@gmail.com>
2019-05-02 14:00:57 -0500

    Remove Unnecessary Config Files

--- config/write_django_secrets.py
+++
@@ -1,9 +0,0 @@
-import os, yaml, string, secrets
-
-# Initialize Configuration File 
-
-config_path = os.path.join('/','home','stev','AboutMe','config','django_secrets.yaml')
-charset = 'abcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*(-_=+)'
-
-with open(config_path, 'w') as config_file:
-    config_file.write(f'    SECRET_KEY: {"".join(secrets.choice(charset) for i in range(50))}\n')
