228d24fc6232d7cc3a55babd84f8559f7731f6b5
Steven Fitzpatrick <stevthedev@gmail.com>
2019-05-03 15:37:34 -0500

    Fix Secret key so the first element is always a letter (legal character)

--- aboutme/aboutme/settings.py
+++ aboutme/aboutme/settings.py
@@ -25,8 +25,9 @@ if not os.path.isfile(config_path):
     os.chmod(config_path,0o640) # Set permissions on file
     shutil.chown(config_path,group='uwsgi')
     with open(config_path, 'w') as config_file: # Write secret key
-        charset = 'abcdefghijklmnopqrstuvwxyz0123456789!@#$^&*(-_=+)'
-        config_file.write( f'SECRET_KEY: {"".join(secrets.choice(charset) for i in range(50))}\n' )
+        charset = 'abcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*(-_=+)'
+        letters = 'abcdefghijklmnopqrstuvwxyz'
+        config_file.write( f'SECRET_KEY: {"".join(secrets.choice(letters) for i in range(1))}{"".join(secrets.choice(charset) for i in range(49))}\n' )
     
 with open(config_path, 'r') as config_file:
     config = yaml.safe_load(config_file)
