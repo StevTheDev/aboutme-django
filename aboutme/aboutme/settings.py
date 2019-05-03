dc31b30529456eadade8f88fe531223ebd04f7a1
Steven Fitzpatrick <stevthedev@gmail.com>
2019-05-03 10:42:50 -0500

    Update Allowed Hosts

--- aboutme/aboutme/settings.py
+++ aboutme/aboutme/settings.py
@@ -36,7 +36,7 @@ with open(config_path, 'r') as config_file:
 SECRET_KEY = config['SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
 
-ALLOWED_HOSTS = ['aboutme-django']
+ALLOWED_HOSTS = ['aboutme-proxy']
 
 # Application definition
 
