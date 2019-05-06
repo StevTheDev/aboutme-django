1476008bebae5719bea0a4dec5b67a786762c310
Steven Fitzpatrick <stevthedev@gmail.com>
2019-05-06 13:03:15 -0500

    Remove export LOCAL_USER_ID from this script - should be done first on docker host before build

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,7 +3,6 @@
 # Add local user
 # Either use the LOCAL_USER_ID if passed in at runtime or
 # fallback
-export LOCAL_USER_ID=id -u
 USER_ID=${LOCAL_USER_ID:-9001}
 
 echo "Starting with UID : $USER_ID"
