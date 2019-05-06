c5ed52f0f6533bc88c54f2e1eb28b8843f060205
Steven Fitzpatrick <stevthedev@gmail.com>
2019-05-06 14:12:59 -0500

    Troubleshooting Permissions

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,7 +6,7 @@
 USER_ID=${LOCAL_USER_ID:-9001}
 
 echo "Starting with UID : $USER_ID"
-useradd --shell /bin/bash -u $USER_ID -o -c "" -m uwsgi
+useradd --shell /bin/bash -u $USER_ID -g $USER_ID -o -c "" -m uwsgi
 export HOME=/home/uwsgi
 
 exec /usr/local/bin/gosu uwsgi "$@"
